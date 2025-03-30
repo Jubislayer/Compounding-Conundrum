91cf886a8832a30d644ac43d3dc2544506833863
Jubislayer <fin.yet@gmail.com>
2025-03-30 13:50:15 +1300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "compound-noun-quiz-v1";
+const STATIC_ASSETS = [
+    "index.html",
+    "styles.css",
+    "game.js",
+    "words.json",
+    "manifest.json",
+    "icon-192.png",
+    "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(STATIC_ASSETS);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
